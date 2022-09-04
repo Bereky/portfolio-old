17b5b2cb387c9add0bd6d74d4b0fe2aeb6b03316
bereky <bereketlemma4@gmail.com>
2022-09-04 05:02:29 +0300

    description edit

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -9,7 +9,7 @@ const Project = () => {
                     <Banner name="Projects"/>
             </div>
                 <div className="project-cards-container"  >
-                    <Card projectHeader={"Shema Marketplace"} projectDescription={"Ecommerce application to purchase fashion clothes"} previewSite={"https://timely-lokum-191a6b.netlify.app/"} sourceSite={"https://github.com/Bereky/mern-ecommerce"}/>
+                    <Card projectHeader={"Shema Marketplace"} projectDescription={"Ecommerce application to purchase fancy clothes"} previewSite={"https://timely-lokum-191a6b.netlify.app/"} sourceSite={"https://github.com/Bereky/mern-ecommerce"}/>
                     <Card projectHeader={"Github Find"} projectDescription={"Search and display github profile"} previewSite={"https://dreamy-otter-ed3b13.netlify.app/"} sourceSite={"https://github.com/Bereky/Github-find"}/>
                     <Card projectHeader={"Express Bus"} projectDescription={"Passenger registration and ticket selling system"} previewSite={false} sourceSite={"https://github.com/Bereky/Express-bus"}/>
                     <Card projectHeader={"የአማርኛ ኪቦርድ"} projectDescription={"A browser based Amharic keyboard"} previewSite={"https://remarkable-dodol-8876e4.netlify.app/"} sourceSite={"https://github.com/Bereky/Amharic-keyboard"}/>
