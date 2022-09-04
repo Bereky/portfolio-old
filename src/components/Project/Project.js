bf01a69264357c00ec3b66f88251da088b7d74b5
bereky <bereketlemma4@gmail.com>
2022-09-04 04:18:28 +0300

    forex project removed

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -13,7 +13,6 @@ const Project = () => {
                     <Card projectHeader={"Github Find"} projectDescription={"Search and display github profile"} previewSite={"https://dreamy-otter-ed3b13.netlify.app/"} sourceSite={"https://github.com/Bereky/Github-find"}/>
                     <Card projectHeader={"Express Bus"} projectDescription={"Passenger registration and ticket selling system"} previewSite={false} sourceSite={"https://github.com/Bereky/Express-bus"}/>
                     <Card projectHeader={"የአማርኛ ኪቦርድ"} projectDescription={"A browser based Amharic keyboard"} previewSite={"https://remarkable-dodol-8876e4.netlify.app/"} sourceSite={"https://github.com/Bereky/Amharic-keyboard"}/>
-                    <Card projectHeader={"Forex Display"} projectDescription={"Foreign Exchange Rate display"} previewSite={"https://cute-shortbread-32a78d.netlify.app/"} sourceSite={"https://github.com/Bereky/Forex-rate"}/>
             </div>
         </div>
      );
