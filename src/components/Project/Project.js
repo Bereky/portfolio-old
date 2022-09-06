65b4b540435d03abc879988b6dbc46d4ff7adc56
bereky <bereketlemma4@gmail.com>
2022-09-06 19:02:56 +0300

    project link modified

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -10,7 +10,7 @@ const Project = () => {
             </div>
                 <div className="project-cards-container"  >
                     <Card projectHeader={"Shema Marketplace"} projectDescription={"Ecommerce application to purchase fancy clothes"} previewSite={"https://timely-lokum-191a6b.netlify.app/"} sourceSite={"https://github.com/Bereky/mern-ecommerce"}/>
-                    <Card projectHeader={"Github Find"} projectDescription={"Search and display github profile"} previewSite={"https://dreamy-otter-ed3b13.netlify.app/"} sourceSite={"https://github.com/Bereky/Github-find"}/>
+                    <Card projectHeader={"Github Find"} projectDescription={"Search and display github profile"} previewSite={"https://eclectic-hamster-b6841d.netlify.app"} sourceSite={"https://github.com/Bereky/Github-find"}/>
                     <Card projectHeader={"Express Bus"} projectDescription={"Passenger registration and ticket selling system"} previewSite={false} sourceSite={"https://github.com/Bereky/Express-bus"}/>
                     <Card projectHeader={"የአማርኛ ኪቦርድ"} projectDescription={"A browser based Amharic keyboard"} previewSite={"https://remarkable-dodol-8876e4.netlify.app/"} sourceSite={"https://github.com/Bereky/Amharic-keyboard"}/>
             </div>
