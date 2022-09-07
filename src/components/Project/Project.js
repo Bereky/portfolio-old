d4c4c247988d35e91effa6c7b2754969b6e75fff
bereky <bereketlemma4@gmail.com>
2022-09-07 19:21:49 +0300

    code formatted

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,18 +1,18 @@
-import './project.css';
 import Card from './Card';
 import Banner from '../Common/Banner';
+import './project.css';
 
 const Project = () => {
     return ( 
         <div className="project-container" >
-                <div className="project-header" id='project'>
-                    <Banner name="Projects"/>
+            <div className="project-header" id='project'>
+                <Banner name="Projects"/>
             </div>
-                <div className="project-cards-container"  >
-                    <Card projectHeader={"Shema Marketplace"} projectDescription={"Ecommerce application to purchase fancy clothes"} previewSite={"https://timely-lokum-191a6b.netlify.app/"} sourceSite={"https://github.com/Bereky/mern-ecommerce"}/>
-                    <Card projectHeader={"Github Find"} projectDescription={"Search and display github profile"} previewSite={"https://eclectic-hamster-b6841d.netlify.app"} sourceSite={"https://github.com/Bereky/Github-find"}/>
-                    <Card projectHeader={"Express Bus"} projectDescription={"Passenger registration and ticket selling system"} previewSite={false} sourceSite={"https://github.com/Bereky/Express-bus"}/>
-                    <Card projectHeader={"የአማርኛ ኪቦርድ"} projectDescription={"A browser based Amharic keyboard"} previewSite={"https://remarkable-dodol-8876e4.netlify.app/"} sourceSite={"https://github.com/Bereky/Amharic-keyboard"}/>
+            <div className="project-cards-container">
+                <Card projectHeader={"Shema Marketplace"} projectDescription={"Ecommerce application to purchase fancy clothes"} previewSite={"https://timely-lokum-191a6b.netlify.app/"} sourceSite={"https://github.com/Bereky/mern-ecommerce"}/>
+                <Card projectHeader={"Github Find"} projectDescription={"Search and display github profile"} previewSite={"https://eclectic-hamster-b6841d.netlify.app"} sourceSite={"https://github.com/Bereky/Github-find"}/>
+                <Card projectHeader={"Express Bus"} projectDescription={"Passenger registration and ticket selling system"} previewSite={false} sourceSite={"https://github.com/Bereky/Express-bus"}/>
+                <Card projectHeader={"የአማርኛ ኪቦርድ"} projectDescription={"A browser based Amharic keyboard"} previewSite={"https://remarkable-dodol-8876e4.netlify.app/"} sourceSite={"https://github.com/Bereky/Amharic-keyboard"}/>
             </div>
         </div>
      );
