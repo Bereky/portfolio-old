72fb378da3303dfea425de93e79fac45178fec15
bereky <bereketlemma4@gmail.com>
2022-09-15 07:04:39 +0300

    loading removed

--- src/app/App.js
+++ src/app/App.js
@@ -6,18 +6,17 @@ import Skill from '../components/Skill/Skill';
 import About from '../components/About/About';
 import Footer from '../components/Common/Footer';
 import Loading from '../components/Common/Loading';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-      setTimeout(() => {setLoading(false)}, 6000)
-  }, []);
 
   return (
     <div className="container">
-      {loading? <Loading /> : <><Nav /><Home /><About /><Project /><Skill /><Footer /></>}
+      <Nav />
+      <Home />
+      <About />
+      <Project />
+      <Skill />
+      <Footer />
     </div>
   );
 }
