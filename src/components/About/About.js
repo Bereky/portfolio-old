1f247c8a72a25157a699527888fb3ca8a2cc48c0
bereky <bereketlemma4@gmail.com>
2022-09-20 10:17:37 +0300

    about update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,10 +13,10 @@ const About = () => {
                 <div className="about-description-container">
                     <h2 className="about-description-header">Software developer, writer and UI/UX designer </h2>
                     <p className='about-description'> 
-                    I am attending Computer Science degree at Jimma University. Currently, my focus is on 
+                    Currently, I am attending Computer Science degree at Jimma University. Besides, I'm focusing on 
                     building and testing user interfaces. I work on responsive design, accessiblity and 
-                    search engine optimization of web pages. I use React and React UI libraries on the front-end,
-                    Nodejs and Express to build API's. Also, I am familiar with different databse technologies like MySQL and MongoDB.
+                    search engine optimization of web pages. I use React and React UI libraries on the front-end.
+                    I build API's with Nodejs and Express. Also, I am familiar with different databse technologies like MySQL and MongoDB.
                     I love to hear from you if you have projects that need creativity,
                     problem solving & programming skills.
                     </p>
