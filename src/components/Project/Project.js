db4999b51e2d426b8c244a464de1662119324059
bereky <bereketlemma4@gmail.com>
2022-10-28 03:16:59 +0300

    added new projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -23,6 +23,17 @@ const Project = () => {
                 stackIcon: "https://www.svgrepo.com/show/331488/mongodb.svg"
             }
         ],
+        decentral: [
+            {
+                stackAlt: "React",
+                stackIcon: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png"
+            },
+            
+            {
+                stackAlt: "Tailwind",
+                stackIcon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png"
+            },
+        ],
         githubFind: [
             {
                 stackAlt: "React",
@@ -66,7 +77,21 @@ const Project = () => {
                 stackAlt: "MySQL",
                 stackIcon: "https://cdn-icons-png.flaticon.com/512/5968/5968313.png"
             }
-        ]
+        ],
+        keyboard: [
+            {
+                stackAlt: "HTML",
+                stackIcon: "https://cdn-icons-png.flaticon.com/512/174/174854.png"
+            },
+            {
+                stackAlt: "CSS",
+                stackIcon: "https://cdn-icons-png.flaticon.com/512/732/732190.png"
+            },
+            {
+                stackAlt: "Bootstrap",
+                stackIcon: "https://raw.githubusercontent.com/themedotid/bootstrap-icon/HEAD/docs/bootstrap-icon-css.png"
+            }
+        ],
     }
     return ( 
         <div className="project-container" >
@@ -75,9 +100,11 @@ const Project = () => {
             </div>
             <div className="project-cards-container">
                 <Card projectHeader={"Shema"} projectDescription={"Full-stack Ecommerce application"} projectStack={projectStackIcons.shema} previewSite={"https://timely-lokum-191a6b.netlify.app/"} sourceSite={"https://github.com/Bereky/mern-ecommerce"}/>
+                <Card projectHeader={"Decentral"} projectDescription={"Blockchain education archive"} projectStack={projectStackIcons.decentral} previewSite={"https://decentral-gray.vercel.app/"} sourceSite={"https://github.com/Bereky/easy_UIs"}/>
                 <Card projectHeader={"Github Find"} projectDescription={"Github profile search engine"} projectStack={projectStackIcons.githubFind} previewSite={"https://eclectic-hamster-b6841d.netlify.app"} sourceSite={"https://github.com/Bereky/Github-find"}/>
                 <Card projectHeader={"Chapa Node"} projectDescription={"Payment gateway implementation boilerplate"} projectStack={projectStackIcons.chapa} previewSite={false} sourceSite={"https://github.com/Bereky/chapa-payment-express"}/>
                 <Card projectHeader={"Express Bus"} projectDescription={"Passenger registration system"} projectStack={projectStackIcons.express} previewSite={false} sourceSite={"https://github.com/Bereky/Express-bus"}/>
+                <Card projectHeader={"Amharic keyboard"} projectDescription={"Web based keyboard to type in Amharic language"} projectStack={projectStackIcons.keyboard} previewSite={"https://remarkable-dodol-8876e4.netlify.app/"} sourceSite={"https://github.com/Bereky/Amharic-keyboard"}/>
             </div>
         </div>
      );
